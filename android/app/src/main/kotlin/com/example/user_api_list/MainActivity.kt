09cc820489069c88c9b7ad9391737cec56631227
Neha <nehatanwar197@gmail.com>
2021-11-17 19:25:15 +0530

    user list through api

---
+++ android/app/src/main/kotlin/com/example/user_api_list/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.user_api_list
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/screens/homepage.dart
@@ -0,0 +1,116 @@
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+
+import 'package:http/http.dart' as http;
+
+class HomePage extends StatefulWidget {
+  HomePage({Key? key}) : super(key: key);
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  List users = [];
+  bool isLoading = false;
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    this.fetchUser();
+  }
+
+  fetchUser() async {
+    setState(() {
+      isLoading = true;
+    });
+    var url = "https://reqres.in/api/users?page=2";
+    var response = await http.get(Uri.parse(url));
+    // print(response.body);
+    if (response.statusCode == 200) {
+      var items = json.decode(response.body)['data'];
+      setState(() {
+        users = items;
+        isLoading = false;
+      });
+    } else {
+      users = [];
+      isLoading = false;
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("List of  Users"),
+      ),
+      body: getBody(),
+    );
+  }
+
+  Widget getBody() {
+    if (users.contains(null) || users.length < 0 || isLoading) {
+      return Center(
+          child: CircularProgressIndicator(
+        valueColor: new AlwaysStoppedAnimation<Color>(Color(0xff4f359b)),
+      ));
+    }
+    return ListView.builder(
+        itemCount: users.length,
+        itemBuilder: (context, index) {
+          return getCard(users[index]);
+        });
+  }
+
+  Widget getCard(item) {
+    var fullName = item['first_name'] + " " + item['last_name'];
+    var email = item['email'];
+    var profileUrl = item['avatar'];
+    return Card(
+      elevation: 1.5,
+      child: Padding(
+        padding: const EdgeInsets.all(10.0),
+        child: ListTile(
+          title: Row(
+            children: <Widget>[
+              Expanded(
+                child: Container(
+                  width: 60,
+                  height: 60,
+                  decoration: BoxDecoration(
+                      color: Color(0xff4f359b),
+                      borderRadius: BorderRadius.circular(60 / 2),
+                      image: DecorationImage(
+                          fit: BoxFit.cover, image: NetworkImage(profileUrl))),
+                ),
+              ),
+              SizedBox(
+                width: 20,
+              ),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  SizedBox(
+                      width: MediaQuery.of(context).size.width - 140,
+                      child: Text(
+                        fullName,
+                        style: TextStyle(fontSize: 17),
+                      )),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  Text(
+                    email.toString(),
+                    style: TextStyle(color: Colors.grey),
+                  )
+                ],
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
